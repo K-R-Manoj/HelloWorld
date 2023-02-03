a9b9a0b08a5345383eaaafc32411576f057fc92b
Manoj Gowda <manojgowdakr28@gmail.com>
2023-02-03 11:22:38 +0530

    fix added log messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+
+  public CreateButton() {
+    console.log("create Button");
+  }
+  public deleteButton() {
+    console.log("delete Button");
+  }
+  public DownloadButton() {
+    console.log("download Button");
+  }
+  public UpdateButton() {
+    console.log("update Button");
+  }
+  public TimepassButton() {
+    console.log("Timepass Button");
+  }
+  public PassTimeButton() {
+    console.log("PassTime Button");
+  }
+  public SameButton1() {
+    console.log("Same Button1");
+  }
+  public SameButton2() {
+    console.log("Same Button2");
+  }
 }
